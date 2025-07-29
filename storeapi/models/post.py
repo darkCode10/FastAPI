5ba2ae3fae977e88ff55d50935e9ea8db1674e7e
MaHSAan10 <125743964+MaHSAan10@users.noreply.github.com>
2025-07-30 00:36:36 +0500

    Adding Comment Resources in the app

--- storeapi/models/post.py
+++ storeapi/models/post.py
@@ -4,3 +4,12 @@ class UserPostIn(BaseModel):
     body:str
 class UserPost(UserPostIn):
     id:int
+
+class CommentIn(BaseModel):
+    body:str
+    post_id:int
+class Comment(CommentIn):
+    id:int
+class UserPostWithComments(BaseModel):
+    post:UserPost
+    comments:list[Comment]
